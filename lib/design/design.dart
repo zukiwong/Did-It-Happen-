ed47e92c88cdbfb902a807a0f3d2a725af1c829f
zuki <wxq839190594@gmail.com>
2026-03-08 16:31:18 +1300

    chore: remove all Flutter leftover files, project is pure Swift/Xcode

--- lib/design/design.dart
+++
@@ -1,9 +0,0 @@
-/// Design system barrel export.
-/// Import this single file to access all tokens and shared components:
-///   import '../../design/design.dart';
-library;
-
-export 'app_colors.dart';
-export 'app_text.dart';
-export 'app_spacing.dart';
-export 'app_button.dart';
--- test/widget_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  // Smoke tests will be added once screens are stable.
-  test('placeholder', () => expect(true, isTrue));
-}
